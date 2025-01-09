05f6c615363ea59a9104aaa64bb2ed1651c809c0
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2025-01-09 18:09:15 +0200

    implement #32

--- src/atoms_base.jl
+++ src/atoms_base.jl
@@ -4,13 +4,31 @@ using Unitful
 
 
 function PairList(ab::AtomsBase.AbstractSystem, cutoff::Unitful.Length; length_unit=unit(cutoff))
-    cell = ustrip.(length_unit, hcat( cell_vectors(ab)... )' )
-    pbc = periodicity(ab)
+    #cell = ustrip.(length_unit, hcat( cell_vectors(ab)... )' )
     r = map( 1:length(ab) ) do i
         # Need to have SVector here for PairList to work
         # if position does not give SVector
         SVector( ustrip.(length_unit, position(ab,i))...)
     end
-    nlist = PairList(r, ustrip(length_unit, cutoff), cell, pbc; int_type=Int)
+    if cell(ab) isa AtomsBase.IsolatedCell{3, <:Any}
+        # this only works in 3D
+        max_x = maximum(q->q[1], r)
+        max_y = maximum(q->q[2], r)
+        max_z = maximum(q->q[3], r)
+        min_x = minimum(q->q[1], r)
+        min_y = minimum(q->q[2], r)
+        min_z = minimum(q->q[3], r)
+        cell_matrix = SMatrix{3,3, typeof(max_x), 9}(
+            # add +1 for cell size
+            max_x - min_x + 1, 0., 0.,
+            0., max_y - min_y + 1, 0.,
+            0., 0., max_z - min_z + 1,
+        )
+    else
+        cell_matrix = ustrip.(length_unit, hcat( cell_vectors(ab)... )' )
+    end
+    pbc = periodicity(ab)
+    
+    nlist = PairList(r, ustrip(length_unit, cutoff), cell_matrix, pbc; int_type=Int)
     return nlist
 end
--- test/test_atoms_base.jl
+++ test/test_atoms_base.jl
@@ -1,4 +1,5 @@
 #using ASEconvert
+using AtomsBase
 using AtomsBuilder
 using NeighbourLists
 using Test
@@ -12,6 +13,25 @@ using Unitful
     id, r = neigs(nlist,1)
 
     @test length(id) == 12
+
+    @testset "AtomsBase Isolated System" begin
+        isys = isolated_system([
+            :H => [0., 0., 0.]u"Å",
+            :H => [0., 0., 2.]u"Å",
+            :H => [0., 10., 0.]u"Å"
+        ])
+        nlist = PairList(isys, 5.0u"Å")
+        id, r = neigs(nlist,1)
+        @test length(id) == 1
+        @test id[1] == 2
+        @test all( r[1] .≈ [0., 0., 2.] )
+        id, r = neigs(nlist,2)
+        @test length(id) == 1
+        @test id[1] == 1
+        @test all( r[1] .≈ [0., 0., -2.] )
+        id, r = neigs(nlist,3)
+        @test length(id) == 0
+    end
 end
 
 
